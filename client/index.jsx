275c36452f76b45193350dd148b96b9877137af8
jasethomson <jasethomson31@gmail.com>
2019-10-30 11:24:26 -0700

    completed react dev environment setup

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ValidatedInput from './components/validated-input';
+
+ReactDOM.render(
+  <ValidatedInput />,
+  document.querySelector("#root")
+)
