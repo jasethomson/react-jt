a165716fdecbedc53d664680b1078b4fe7dc16e8
jasethomson <jasethomson31@gmail.com>
2019-11-08 07:38:27 -0800

    Added top 3 subscription table

--- client/components/mainsub.jsx
+++ client/components/mainsub.jsx
@@ -13,54 +13,90 @@ class MainSub extends React.Component{
 
   render(){
     return (
-      <Table dark>
-        <thead>
-          <tr>
-            <th>Subscription</th>
-            <th>Monthly</th>
-            <th>YTD</th>
-            <th>Next PMT due</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Netflix</td>
-            <td>$10.99</td>
-            <td>$120.89</td>
-            <td>12/1/2019</td>
-          </tr>
-          <tr>
-            <td>Amazon Prime</td>
-            <td>$12.99</td>
-            <td>$142.89</td>
-            <td>12/6/2019</td>
-          </tr>
-          <tr>
-            <td>Spotify</td>
-            <td>$9.99</td>
-            <td>$98.89</td>
-            <td>12/3/2019</td>
-          </tr>
-          <tr>
-            <td>Netflix</td>
-            <td>$10.99</td>
-            <td>$120.89</td>
-            <td>12/1/2019</td>
-          </tr>
-          <tr>
-            <td>Amazon Prime</td>
-            <td>$12.99</td>
-            <td>$142.89</td>
-            <td>12/6/2019</td>
-          </tr>
-          <tr>
-            <td>Spotify</td>
-            <td>$9.99</td>
-            <td>$98.89</td>
-            <td>12/3/2019</td>
-          </tr>
-        </tbody>
-      </Table>
+      <div className="container">
+        <div className="row">
+          <Table dark>
+            <thead>
+              <tr>
+                <th>Subscription</th>
+                <th>Monthly</th>
+                <th>YTD</th>
+                <th>Next PMT due</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Netflix</td>
+                <td>$10.99</td>
+                <td>$120.89</td>
+                <td>12/1/2019</td>
+              </tr>
+              <tr>
+                <td>Amazon Prime</td>
+                <td>$12.99</td>
+                <td>$142.89</td>
+                <td>12/6/2019</td>
+              </tr>
+              <tr>
+                <td>Spotify</td>
+                <td>$9.99</td>
+                <td>$98.89</td>
+                <td>12/3/2019</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+        <div className="row">
+          <Table dark>
+            <thead>
+              <tr>
+                <th>Subscription</th>
+                <th>Monthly</th>
+                <th>YTD</th>
+                <th>Next PMT due</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Netflix</td>
+                <td>$10.99</td>
+                <td>$120.89</td>
+                <td>12/1/2019</td>
+              </tr>
+              <tr>
+                <td>Amazon Prime</td>
+                <td>$12.99</td>
+                <td>$142.89</td>
+                <td>12/6/2019</td>
+              </tr>
+              <tr>
+                <td>Spotify</td>
+                <td>$9.99</td>
+                <td>$98.89</td>
+                <td>12/3/2019</td>
+              </tr>
+              <tr>
+                <td>Netflix</td>
+                <td>$10.99</td>
+                <td>$120.89</td>
+                <td>12/1/2019</td>
+              </tr>
+              <tr>
+                <td>Amazon Prime</td>
+                <td>$12.99</td>
+                <td>$142.89</td>
+                <td>12/6/2019</td>
+              </tr>
+              <tr>
+                <td>Spotify</td>
+                <td>$9.99</td>
+                <td>$98.89</td>
+                <td>12/3/2019</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </div>
     )
   }
 }
