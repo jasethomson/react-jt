48284b585fea817f88adff53e13dcde07d328fa0
jasethomson <jasethomson31@gmail.com>
2019-11-08 07:34:29 -0800

    removed the numeric index for subscription tables

--- client/components/mainsub.jsx
+++ client/components/mainsub.jsx
@@ -16,7 +16,6 @@ class MainSub extends React.Component{
       <Table dark>
         <thead>
           <tr>
-            <th>#</th>
             <th>Subscription</th>
             <th>Monthly</th>
             <th>YTD</th>
@@ -25,42 +24,36 @@ class MainSub extends React.Component{
         </thead>
         <tbody>
           <tr>
-            <th scope="row">1</th>
             <td>Netflix</td>
             <td>$10.99</td>
             <td>$120.89</td>
             <td>12/1/2019</td>
           </tr>
           <tr>
-            <th scope="row">2</th>
             <td>Amazon Prime</td>
             <td>$12.99</td>
             <td>$142.89</td>
             <td>12/6/2019</td>
           </tr>
           <tr>
-            <th scope="row">3</th>
             <td>Spotify</td>
             <td>$9.99</td>
             <td>$98.89</td>
             <td>12/3/2019</td>
           </tr>
           <tr>
-            <th scope="row">1</th>
             <td>Netflix</td>
             <td>$10.99</td>
             <td>$120.89</td>
             <td>12/1/2019</td>
           </tr>
           <tr>
-            <th scope="row">2</th>
             <td>Amazon Prime</td>
             <td>$12.99</td>
             <td>$142.89</td>
             <td>12/6/2019</td>
           </tr>
           <tr>
-            <th scope="row">3</th>
             <td>Spotify</td>
             <td>$9.99</td>
             <td>$98.89</td>
